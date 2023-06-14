4aa4c8cd0b9708bdfad25cc908a287709662c5ad
LaiaRuizM <laia.ruizm@hotmail.com>
2023-06-14 17:17:56 +0200

    feat: update CalcPage to return string param values

--- sanz-clima-next-js-13-app/app/calc/page.jsx
+++ sanz-clima-next-js-13-app/app/calc/page.jsx
@@ -1,3 +1,10 @@
-export default function CalcPage() {
-  return <h1>Calculate page test</h1>;
+CalcPage.getInitialProps = async ctx => {
+  return ctx.req.url;
+};
+
+export default function CalcPage(url) {
+  const paramValues = Object.values(url.searchParams);
+  const stringParamValues = JSON.stringify(paramValues);
+
+  return <h1>Calculate page test: {stringParamValues}</h1>;
 }
