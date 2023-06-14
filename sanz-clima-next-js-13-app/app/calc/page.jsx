bde1f93c31fde979b08c72d88c9c52cb630f1cfd
LaiaRuizM <laia.ruizm@hotmail.com>
2023-06-14 22:29:08 +0200

    feat: display calc result

--- sanz-clima-next-js-13-app/app/calc/page.jsx
+++ sanz-clima-next-js-13-app/app/calc/page.jsx
@@ -6,5 +6,26 @@ export default function CalcPage(url) {
   const paramValues = Object.values(url.searchParams);
   const stringParamValues = JSON.stringify(paramValues);
 
-  return <h1>Calculate page test: {stringParamValues}</h1>;
+  const calculateSum = () => {
+    let calculate = 0.0;
+
+    if (Array.isArray(paramValues)) {
+      paramValues.forEach(value => {
+        const number = parseFloat(value);
+        if (!isNaN(number)) {
+          calculate += number;
+        }
+      });
+    }
+
+    return calculate;
+  };
+
+  const calculate = calculateSum();
+
+  return (
+    <div>
+      <p>Sum of numerical arguments and avoiding other types: {calculate}</p>
+    </div>
+  );
 }
